d804c1fe2f432ece594255c9476215654a366d7b
Camila Fariña <Camilafarina03@gmail.com>
2023-05-13 21:50:34 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -67,7 +67,23 @@ $(document).ready(() => {
         spanEmail.html(' ' + email + ' ');
       }
 
-
+      
+      var swiper = new Swiper(".cards-info", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        centerSlide: 'true',
+        fade: 'true',
+        grabCursor: 'true',
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 
 
 });
